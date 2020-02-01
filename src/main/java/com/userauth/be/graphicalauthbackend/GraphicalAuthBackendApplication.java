aa9ca1d01238e06eef467e027154e99574ab511f
dilsfernando <dilsfernando777@gmail.com>
2020-02-01 22:20:27 +0530

    add backend spring boot

---
+++ src/main/java/com/userauth/be/graphicalauthbackend/GraphicalAuthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.userauth.be.graphicalauthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphicalAuthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphicalAuthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userauth/be/graphicalauthbackend/GraphicalAuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userauth.be.graphicalauthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicalAuthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
